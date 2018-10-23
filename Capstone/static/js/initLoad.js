c5ce8b750590963afb0e9782fe745cd265047dba
areakryte <ydavidf@gmail.com>
2018-10-22 22:19:27 -0700

    Textures, Fonts, and CSS touches

--- Capstone/static/js/initLoad.js
+++ Capstone/static/js/initLoad.js
@@ -12,34 +12,6 @@ var CAD = "";
 var JPY = "";
 var USD = "";
 
-// $('document').ready(function(){
-//     //alert(10);
-
-//     var modalString = "<div class=\"modal fade\" id=\"exampleModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">" +
-//     "<div class=\"modal-dialog\" role=\"document\">" +
-//       "<div class=\"modal-content\">" +
-//         "<div class=\"modal-header\">" +
-//           "<h5 class=\"modal-title\" id=\"exampleModalLabel\">Modal title</h5>" +
-//           "<button type=\"button\ class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">" +
-//             "<span aria-hidden=\"true\">&times;</span>" +
-//           "</button>" +
-//         "</div>" +
-//         "<div class=\"modal-body\">" +
-//           "..." +
-//         "</div>" +
-//         "<div class=\"modal-footer\">" +
-//           "<button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">Close</button>" +
-//           "<button type=\"button\" class=\"btn btn-primary\">Save changes</button>" +
-//         "</div>" +
-//       "</div>" +
-//     "</div>" +
-//   "</div>";
-
-//     var newSection = "<div class=\"currency\"><p>Test</p></div>"
-
-//     $("body").append(modalString);
-// })
-
 $('document').ready(function(){
     var initWidth = $('.currency div .currency-body').css(["width"]);
     
@@ -78,15 +50,12 @@ $("select").change(function(){
     var rateTo = 0;
 
     if(baseCurrency !== compareCurrency){
-        rateTo = parseFloat((amount*worldCurrency[compareCurrency])/worldCurrency[baseCurrency]);
+        rateTo = parseFloat((amount*worldCurrency[compareCurrency])/worldCurrency[baseCurrency]).toFixed(2);
         $('.curr-output').text(parseFloat(rateTo)); 
     }
     else{
         $('.curr-output').text(parseFloat($(".curr-input").val())); 
     }
-
-    
-    
 });
 
 function addCurrency(){
